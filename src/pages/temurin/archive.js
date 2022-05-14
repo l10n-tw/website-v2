7a38eb880e5792dcc0ff9dc4072638c83c244c5f
toto6038 <50100922+toto6038@users.noreply.github.com>
2022-05-14 13:15:20 +0800

    Extract strings from temurin/archive.js for localizing

--- src/pages/temurin/archive.js
+++ src/pages/temurin/archive.js
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import { Link, graphql } from 'gatsby'
 import { FaArrowCircleRight } from 'react-icons/fa'
-
+import { useTranslation } from 'gatsby-plugin-react-i18next'
 import Layout from '../../components/Layout'
 import Seo from '../../components/Seo'
 import VersionSelector from '../../components/VersionSelector'
@@ -9,41 +9,45 @@ import ChecksumModal from '../../components/ChecksumModal'
 import TemurinArchiveTable from '../../components/TemurinArchiveTable'
 import { getAssetsForVersion } from '../../hooks'
 
-const TemurinReleases = () => (
-  <Layout>
-    <Seo title='Archive' />
-    <section className='py-5 text-center container'>
-      <div className='row py-lg-5'>
-        <div className='col-lg-10 col-md-8 mx-auto'>
-          <h1 className='fw-light'>Archive</h1>
-          <div className='row align-items-center pt-2'>
-            <div className='callout callout-default text-start'>
-              Please be aware that this archive contains old releases and intermediate builds created as a development step towards a <Link to='/temurin/releases'>full release</Link>.
-              Using old, superseded, or otherwise unsupported builds is not recommended. Intermediate builds are ephemeral, and may disappear in the future.
-              <br />
-              <br />
-              The following notice applies to intermediate builds:
-              <br />
-              "This is an intermediate build made available for testing purposes only. The code is untested and presumed incompatible with the Java SE specification.
-              You should not deploy or write to this code, but instead use the tested and certified Java SE compatible version of the code that is available at <a href='https://adoptium.net'>adoptium.net</a>.
-              Redistribution of any Intermediate Build must retain this notice."
-            </div>
-            <div className='btn-group'>
-              <Link to='/temurin/releases' className='btn btn-primary m-3'>
-                Latest releases <FaArrowCircleRight />
-              </Link>
-              <Link to='/temurin/nightly' className='btn btn-secondary m-3'>
-                Nightly builds <FaArrowCircleRight />
-              </Link>
+const TemurinReleases = () => {
+  const { t } = useTranslation()
+  return (
+    <Layout>
+      <Seo title='Archive' />
+      <section className='py-5 text-center container'>
+        <div className='row py-lg-5'>
+          <div className='col-lg-10 col-md-8 mx-auto'>
+            <h1 className='fw-light'>{t('archive_title')}</h1>
+            <div className='row align-items-center pt-2'>
+              <div className='callout callout-default text-start'>{t('archive_notice_text1')}
+                <Link to='/temurin/releases'>{t('archive_link_to_releases')}</Link>{t('archive_notice_text2')}{t('archive_notice_text3')}
+                <br />
+                <br />
+                {t('archive_notice_text4')}
+                <br />
+                {t('archive_notice_text5')}
+                <a href='https://adoptium.net'>{t('archive_link_to_home')}</a>{t('archive_notice_text6')}{t('archive_notice_text7')}
+              </div>
+              <div className='btn-group'>
+                <Link to='/temurin/releases' className='btn btn-primary m-3'>
+                  {t('archive_latest_releases_btn')} <FaArrowCircleRight />
+                </Link>
+                <Link to='/temurin/nightly' className='btn btn-secondary m-3'>
+                  {t('archive_nightly_builds_btn')} <FaArrowCircleRight />
+                </Link>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <VersionSelector updater={getAssetsForVersion} releaseType='ga' Table={TemurinArchiveTable} />
-      <ChecksumModal />
-    </section>
-  </Layout>
-)
+        <VersionSelector updater={getAssetsForVersion} releaseType='ga' Table={TemurinArchiveTable} />
+        <ChecksumModal />
+      </section>
+    </Layout>
+
+  )
+}
+
+
 
 export default TemurinReleases
 
